c8f4df6088d18a6d966b31f893ca6546098f9868
ga1 <1399499020@qq.com>
2022-12-05 16:30:28 +0800

    2022/12/5:16.30

--- app.py
+++ app.py
@@ -1,14 +1,36 @@
 from flask import Flask,render_template,request
-
+import pymysql
 app = Flask(__name__)
-
+def sqlcon():
+    con=pymysql.connect(
+        host='localhost',
+        port=3306,
+        user='root',
+        password='123123',
+        database='user'
+    )
+    return con
 
 @app.route('/')
 def login():  # put application's code here
     return render_template('pages-login.html')
-@app.route('/logins')
+@app.route('/logins',methods=['POST','GET'])
 def ss():
-    return "ac"
+    password=(request.values.get('password'))
+    username=request.values.get('username')
+
+    #在数据库中查询
+    con=sqlcon()
+    cursor=con.cursor()
+    sql="select * from user where username='{}' and password = '{}'".format(username,password)
+    print("select * from user where username='{}' and password = '{}'".format(username,password))
+    cursor.execute(sql)
+    res = cursor.fetchone()
+    if res is None:
+        return 'no'
+    else:
+        return 'yes'
+
 @app.route('/pages-register.html')
 def reg():
     return render_template('pages-register.html')
