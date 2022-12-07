2b87ae514f863135fb797ddb5b0840f940c027fa
ga1 <1399499020@qq.com>
2022-12-07 15:50:46 +0800

    增删查

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def GetData():
     cursor.execute('SELECT * FROM `user`')
     data = cursor.fetchall()
     return data
+def gai():
+    pass
+def shan():
+    pass
+
+
 @app.route('/')
 def login():  # put application's code here
     return render_template('pages-login.html')
@@ -65,12 +71,44 @@ def reg():
 @app.route('/update',methods=['POST','GET'])
 def update():
     id=request.values.get('id')
-    name=request.values.get('username')
+    name=request.values.get('name')
     password=request.values.get('password')
     email=request.values.get('email')
-    if request.values.get('targ') =="YES":
-        return "yes"
+    targ=request.values.get('targ')
+    if targ=="insert":
+        print(name,password,email)
+        con = sqlcon()
+        cursor = con.cursor()
+        sql = "insert into user values ('0','{}','{}','{}') ".format(name,password,email)
+        cursor.execute(sql)
+        con.commit()
+        return "insert"
+    if targ=="delete":
+        print(id,name, password, email)
+        con = sqlcon()
+        cursor = con.cursor()
+        sql = "DELETE FROM `user` WHERE id={}".format(id)
+        cursor.execute(sql)
+        con.commit()
+        return "delete"
+    if request.values.get('targ') =="update":
+            return render_template('forms-layouts.html', id=id, name=name, password=password, email=email)
+
+
     return render_template('forms-layouts.html',id=id,name=name,password=password,email=email)
+
+@app.route('/seach',methods=['post','get'])
+def seach():
+    id = request.values.get('id')
+    con = sqlcon()
+    cursor = con.cursor()
+    cursor.execute('SELECT * FROM `user`WHERE id = {}'.format(id))
+    data = cursor.fetchone()
+    res='id= {} , \n   name= {},  \n  password={},  \n  email={}'.format(data[0],data[1],data[2],data[3])
+    return render_template('forms-layouts.html',id=data[0],name=data[1],password=data[2],email=data[3])
+
+
+
 
 if __name__ == '__main__':
     app.run()
